dfec0e23a744fe6b2aa8f75b5cce9301251a0fce
CoderSparks <codersparks@gmail.com>
2020-10-24 16:37:15 +0100

    Attempting to get event genreation to work

--- src/SSDPEvent.ts
+++ src/SSDPEvent.ts
@@ -19,39 +19,48 @@ export class SSDPEvent {
     logger.debug("Response Lines:" + responseLines);
 
     responseLines.forEach((line: string) => {
-      const [header, value] = line.split(":", 2);
+      let header_value:string[] = line.split(":", 2);
 
-      logger.debug("Header: " + header + " value: " + value);
+      if(header_value.length == 2) {
 
-      // switch (header) {
-      //   case "HOST":
-      //     this.host = value.trim();
-      //     break;
-      //   case "LOCATION":
-      //     this.location = value.trim();
-      //     break;
-      //   case "OPT":
-      //     this.opt = value.trim();
-      //     break;
-      //   case "NT":
-      //     this.nt = value.trim();
-      //     break;
-      //   case "NTS":
-      //     this.nts = value.trim();
-      //     break;
-      //   case "SERVER":
-      //     this.server = value.trim();
-      //     break;
-      //   case "USN":
-      //     this.usn = value.trim();
-      //     break;
-      //   case "X-User-Agent":
-      //     this.userAgent = value.trim();
-      //     break;
-      //   default:
-      //     this.other.push({ name: header.trim(), value: value.trim() });
-      //     break;
-      // }
+        let header:string = header_value[0];
+        let value:string = header_value[1];
+
+        logger.debug("Header: " + header + " value: " + value);
+
+        switch (header) {
+          case "HOST":
+            this.host = value.trim();
+            break;
+          case "LOCATION":
+            this.location = value.trim();
+            break;
+          case "OPT":
+            this.opt = value.trim();
+            break;
+          case "NT":
+            this.nt = value.trim();
+            break;
+          case "NTS":
+            this.nts = value.trim();
+            break;
+          case "SERVER":
+            this.server = value.trim();
+            break;
+          case "USN":
+            this.usn = value.trim();
+            break;
+          case "X-User-Agent":
+            this.userAgent = value.trim();
+            break;
+          default:
+            this.other.push({ name: header.trim(), value: value.trim() });
+            break;
+        }
+      } else {
+        logger.debug("Not of header:value format, adding to other field");
+        this.other.push({ name: header_value[0], value: ""})
+      }
     });
   }
 }
